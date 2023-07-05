b9612e2878fb5a1675a8f6ae347f7fa4cb15a3a4
Nalugya31 <136578468+Nalugya31@users.noreply.github.com>
2023-07-05 08:00:24 +0300

    i have created a file called i_o2.py

--- i_02.py
+++ i_02.py
@@ -1,11 +1,16 @@
 #age(year of birth),location and type of work
 def payee(name,salary,age,location,work):
+    
+    
     rate = 0.3
     if salary>=300000:
       tax = rate*salary
       net = salary-tax
       print("*************")
       print("Dear:", name)
+      print("You'are: ", age,"years")
+      print("Your district is: ", location)
+      print("Your work type is: ", work)
       print("Your payable tax is: ",tax )
       print("And your total salary is: ", net)
       print(".....................")
@@ -16,11 +21,12 @@ def payee(name,salary,age,location,work):
 
 
 
-print("Your welcome! Please follow the instructions ") 
-name = raw_input("Please enter your name: ")
-age = input("Please enter your age: ")
-location = raw_input("Please enter location: ")
-work = raw_input("Please enter your work type here: ")
-salary = input("please enter your salary: ")
+print("Your welcome! Please follow the instructions ") #This will come on top of the page.
+name = raw_input("Please enter your name: ")# prompt the user to enter their name.
+year_of_birth = input("Please enter your year of birth: ")# prompt the user to enter their year of birth.
+age = 2023-year_of_birth # This will help us determine the age of the user.
+location = raw_input("Please enter location: ") # This will help us determine the location of the user.
+work = raw_input("Please enter your work type here: ") # This will help us determine the work type of the user.
+salary = input("please enter your salary: ")# prompt the user to enter their salary.
 
 payee(name,salary,age,location,work)   
